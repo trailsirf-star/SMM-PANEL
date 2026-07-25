9a37e2925a4145d1b8205e3eb9bf76e25004eb66
trailsirf-star <trailsirf@gmail.com>
2026-07-25 09:08:29 +0500

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-
-router.use(protect);
-router.get('/', (req, res) => res.render('dashboard'));
-
-module.exports = router;
