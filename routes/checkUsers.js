010aa08cd70a1f4f3a38ec415c1ddbdf96ee0df6
trailsirf-star <trailsirf@gmail.com>
2026-07-25 12:12:35 +0500

    Update checkUsers.js

--- routes/checkUsers.js
+++ routes/checkUsers.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = require('./models/User');
+const User = require('../models/User');
 
 async function run() {
   await mongoose.connect(process.env.MONGO_URI);
